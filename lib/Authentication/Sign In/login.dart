2ada7ddeaf5d699f0f9e1f8705d09b0ae3ed023e
kshitiz <kshitizagarwal3133@gmail.com>
2024-04-05 21:12:25 +0530

    latest

--- lib/Authentication/Sign In/login.dart
+++ lib/Authentication/Sign In/login.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ftbwallet/Animation/wave.dart';
+import 'package:ftbwallet/homePage.dart';
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -13,24 +15,26 @@ class Login extends StatelessWidget {
           Text("Login",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 100,
-                  child: SvgPicture.asset("assets/Icon/indian.svg",color: Colors.black,),
-                ),
-                Text("Face Unlock",style: TextStyle(fontSize: 20),),
-                Text("शकल से लॉगिन करें",style: TextStyle(fontSize: 20),),
-                SizedBox(
-                  height: 100,
-                  child: Transform.scale(scale: 2,child: SvgPicture.asset("assets/Icon/fingerprint.svg",color: Colors.black,)),
-                ),
-                Text("Fingerprint Unlock",style: TextStyle(fontSize: 20),),
-                Text("शकल से लॉगिन करें",style: TextStyle(fontSize: 20),)
-              ],
+            child: GestureDetector(
+              onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));},
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: SvgPicture.asset("assets/Icon/indian.svg",color: Colors.black,),
+                  ),
+                  Text("Face Unlock",style: TextStyle(fontSize: 20),),
+                  Text("शकल से लॉगिन करें",style: TextStyle(fontSize: 20),),
+                  SizedBox(
+                    height: 100,
+                    child: Transform.scale(scale: 2,child: SvgPicture.asset("assets/Icon/fingerprint.svg",color: Colors.black,)),
+                  ),
+                  Text("Fingerprint Unlock",style: TextStyle(fontSize: 20),),
+                  Text("शकल से लॉगिन करें",style: TextStyle(fontSize: 20),)
+                ],
+              ),
             ),
           ),
-
         ],
       ),
     );
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -44,7 +44,7 @@ class _HomePageState extends State<HomePage> {
             _currentPageIndex = index;
             _pageController.animateToPage(
               index,
-              duration: Duration(milliseconds: 500),
+              duration: Duration(milliseconds: 200),
               curve: Curves.easeInOut,
             );
           });
